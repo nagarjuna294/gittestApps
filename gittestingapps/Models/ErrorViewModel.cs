a2795dad5132cbdbe6aeb4aad842bc2e3b2b6a5e
Nagarjuna J <nagarjunaj33@gmail.com>
2024-07-04 10:41:28 +0530

    init setup for main branch

---
+++ gittestingapps/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gittestingapps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
